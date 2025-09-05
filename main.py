4a754ed058a5dd9e40a2646dd039efb76b01fbfb
branles14 <135960981+branles14@users.noreply.github.com>
2025-09-05 14:20:49 -0600

    feat: allow configurable port

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,4 +10,9 @@ async def health():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+    )
