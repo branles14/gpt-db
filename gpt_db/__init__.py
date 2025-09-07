137c6d9e11ea2382a2a25ae6e31df8b1ddb4be15
branles14 <branles@pm.me>
2025-09-07 14:31:49 -0600

    fixed import loop

--- gpt_db/__init__.py
+++ gpt_db/__init__.py
@@ -1,4 +1,9 @@
-from .app import app, create_app
+"""gpt_db package init.
 
-__all__ = ["app", "create_app"]
+Avoid re-exporting the FastAPI application object here to prevent
+import-order cycles when the serverless runtime imports `gpt_db.app`
+as the entrypoint while submodules import from `gpt_db`.
+"""
+
+__all__ = []
 
